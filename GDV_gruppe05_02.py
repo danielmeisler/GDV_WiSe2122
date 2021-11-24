d0edfa4d56761f8c03d99db27ed372a5e3b6a79c
Daniel Meisler <daniel_meisler@web.de>
2021-11-24 12:41:51 +0100

    merge

--- GDV_gruppe05_02.py
+++ GDV_gruppe05_02.py
@@ -21,197 +21,152 @@ num_green = 2
 num_red = 6
 gt_list = (num_red, num_green, num_blue, num_yellow, num_white, num_pink)
 
-# define color ranges in HSV, note that OpenCV uses the following ranges H: 0-179, S: 0-255, V: 0-255
+# configration of the colors
+# name, hue, hue_range, saturation, saturation_range, value, value_range, kernel_size, kernel_shape, morph_operation, morph_iteration
+blue = ('blue', 97, 4, 175, 100, 135, 100, 3, cv2.MORPH_ELLIPSE, 'none')
+green = ('green', 41, 6, 245, 100, 135, 100, 3, cv2.MORPH_ELLIPSE, 'none')
+yellow = ('yellow', 28, 5, 255, 100, 200, 60, 2, cv2.MORPH_ELLIPSE, 'none')
+red = ('red', 173, 15, 115, 100, 170, 100, 4, cv2.MORPH_ELLIPSE,'morph_red')
+pink = ('pink',0, 9, 45, 100, 255, 100, 2, cv2.MORPH_ELLIPSE,'morph_pink')
+white =('white', 29, 6, 0, 100, 255, 100, 2, cv2.MORPH_ELLIPSE,'morph_white')
 
-# red
-red_bgr = (30, 28, 94)
-red_pixel = np.zeros((1,1,3), np.uint8)
-red_pixel[0][0] = red_bgr
-red_hsv = cv2.cvtColor(red_pixel, cv2.COLOR_BGR2HSV)
-
-# green
-green_bgr = (40, 150, 90)
-green_pixel = np.zeros((1,1,3), np.uint8)
-green_pixel[0][0] = green_bgr
-green_hsv = cv2.cvtColor(green_pixel, cv2.COLOR_BGR2HSV)
-
-# blue
-blue_bgr = (174, 128, 15)
-blue_pixel = np.zeros((1,1,3), np.uint8)
-blue_pixel[0][0] = blue_bgr
-blue_hsv = cv2.cvtColor(blue_pixel, cv2.COLOR_BGR2HSV)
-
-# yellow
-yellow_bgr = (45, 220, 254)
-yellow_pixel = np.zeros((1,1,3), np.uint8)
-yellow_pixel[0][0] = yellow_bgr
-yellow_hsv = cv2.cvtColor(yellow_pixel, cv2.COLOR_BGR2HSV)
-
-# white
-white_bgr = (188, 225, 255)
-white_pixel = np.zeros((1,1,3), np.uint8)
-white_pixel[0][0] = white_bgr
-white_hsv = cv2.cvtColor(white_pixel, cv2.COLOR_BGR2HSV)
-
-# pink
-pink_bgr = (64, 64, 162)
-pink_pixel = np.zeros((1,1,3), np.uint8)
-pink_pixel[0][0] = pink_bgr
-pink_hsv = cv2.cvtColor(pink_pixel, cv2.COLOR_BGR2HSV)
-
-# morphological operations
-# optional mapping of values with morphological shapes
-
-
-def morph_shape(val):
-    if val == 0:
-        return cv2.MORPH_RECT
-    elif val == 1:
-        return cv2.MORPH_CROSS
-    elif val == 2:
-        return cv2.MORPH_ELLIPSE
+# all colors in one array
+color_configuration = (red,green,blue,yellow,white,pink)
 
 # dilation with parameters
-
-
 def dilatation(img, size, shape):
     element = cv2.getStructuringElement(shape, (2 * size + 1, 2 * size + 1),
-                                       (size, size))
+                                        (size, size))
     return cv2.dilate(img, element)
 
 # erosion with parameters
-
-
 def erosion(img, size, shape):
     element = cv2.getStructuringElement(shape, (2 * size + 1, 2 * size + 1),
-                                       (size, size))
+                                        (size, size))
     return cv2.erode(img, element)
 
 # opening
-
-
 def opening(img, size, shape):
     element = cv2.getStructuringElement(shape, (2 * size + 1, 2 * size + 1),
-                                       (size, size))
+                                        (size, size))
     return cv2.morphologyEx(img, cv2.MORPH_OPEN, element)
 
 # closing
-
-
 def closing(img, size, shape):
     element = cv2.getStructuringElement(shape, (2 * size + 1, 2 * size + 1),
-                                       (size, size))
+                                        (size, size))
     return cv2.morphologyEx(img, cv2.MORPH_CLOSE, element)
 
-#setting the parameters
-kernel_shape = morph_shape(2)
-kernel_size = 3
-
-# set color under test
-color_names = ['red', 'green', 'blue', 'yellow', 'white', 'pink']
-color_hsv = [red_hsv, green_hsv, blue_hsv, yellow_hsv, white_hsv, pink_hsv]
-num_colors = len(color_hsv)
-
-# setting the parameters that work for all colors
-hue_range = 8
-saturation_range = 100
-value_range = 100
-
-# set individual (per color) parameters
-
 num_test_images_succeeded = 0
-for img_name in glob.glob('images/chewing_gum_balls*.jpg'):
+for img_name in glob.glob('Images/chewing_gum_balls*.jpg'):
     # load image
     print('Searching for colored balls in image:', img_name)
 
     all_colors_correct = True
-
-    for c in range(0, num_colors):
-
+    for c in range(0, len(color_configuration)):
         img = cv2.imread(img_name, cv2.IMREAD_COLOR)
         height = img.shape[0]
         width = img.shape[1]
 
-        # TODO: Insert your algorithm here
-        lower_color = np.array([color_hsv[c][0][0][0] - hue_range,color_hsv[c][0][0][1] - saturation_range,color_hsv[c][0][0][2] - value_range])
-        upper_color = np.array([color_hsv[c][0][0][0] + hue_range,color_hsv[c][0][0][1] + saturation_range,color_hsv[c][0][0][2] + value_range])
-
         # convert to HSV
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+        # TODO: Insert your algorithm here
+        lower_color = np.array([color_configuration[c][1] - color_configuration[c][2], 
+                                color_configuration[c][3] - color_configuration[c][4], 
+                                color_configuration[c][5] - color_configuration[c][6]])
+        upper_color = np.array([color_configuration[c][1] + color_configuration[c][2], 
+                                color_configuration[c][3] + color_configuration[c][4], 
+                                color_configuration[c][5] + color_configuration[c][6]])
         # create a mask
         mask = cv2.inRange(hsv, lower_color, upper_color)
-        mask = opening(mask,kernel_size,kernel_shape)
-        mask = closing(mask,kernel_size,kernel_shape)
-
+        # specific morph
+        if(color_configuration[c][9] == "morph_red"):
+            for i in range(3):
+                mask = dilatation(mask, color_configuration[c][7], color_configuration[c][8])
+            for i in range(4):
+                mask = erosion(mask, color_configuration[c][7], color_configuration[c][8])
+        if(color_configuration[c][9] == "morph_white"):
+            for i in range(2):
+                mask = dilatation(mask, color_configuration[c][7], color_configuration[c][8])
+            for i in range(3):
+                mask = erosion(mask, color_configuration[c][7], color_configuration[c][8])
+        if(color_configuration[c][9] == "morph_pink"):
+            #for i in range(1):
+            #    mask = dilatation(mask, color_configuration[c][7], color_configuration[c][8])
+            for i in range(1):
+                mask = erosion(mask, color_configuration[c][7], color_configuration[c][8])
+        # generel morph
+        mask = opening(mask, color_configuration[c][7], color_configuration[c][8])
+        # variables for counting
         connectivity = 8
-        (num_labels, labels, stats, centroids) = cv2.connectedComponentsWithStats(mask,connectivity,cv2.CV_32S)
+        (num_labels, labels, stats, centroids) = cv2.connectedComponentsWithStats(
+                                                    mask, connectivity, cv2.CV_32S)
 
         # TODO: implement something to set this variable
         min_size = 10
         num_rejected = 1
         num_final_labels = num_labels-num_rejected
+        roundness = 0
 
         # go through all (reasonable) found connected components
-        for i in range(1,num_labels):
-          # check size and roundness as plausibility
-          x = stats[i, cv2.CC_STAT_LEFT]
-          y = stats[i, cv2.CC_STAT_TOP]
-          w = stats[i, cv2.CC_STAT_WIDTH]
-          h = stats[i, cv2.CC_STAT_HEIGHT]
-          if w < min_size or h < min_size:
-            print ('Found a too small component.')
-            num_rejected += 1
-            continue # found component is too small to be correct 
-          if w > h:
-            roundness = 1.0 / (w/h)
-          elif h > w:
-            roundness = 1.0 / (h/w)  
-          if (roundness < .5):
-            print ('Found a component that is not round enough.')
-            num_rejected += 1
-            continue # ratio of width and height is not suitable
+        for i in range(1, num_labels):
+            # check size and roundness as plausibility
+            x = stats[i, cv2.CC_STAT_LEFT]
+            y = stats[i, cv2.CC_STAT_TOP]
+            w = stats[i, cv2.CC_STAT_WIDTH]
+            h = stats[i, cv2.CC_STAT_HEIGHT]
+            if w < min_size or h < min_size:
+                print('Found a too small component.')
+                num_rejected += 1
+                continue  # found component is too small to be correct
+            if w > h:
+                roundness = 1.0 / (w/h)
+            elif h > w:
+                roundness = 1.0 / (h/w)
+            if (roundness < .5):
+                print('Found a component that is not round enough.')
+                num_rejected += 1
+                continue  # ratio of width and height is not suitable
 
-        # find and draw center
-        center = centroids[c]
-        center = np.round(center)
-        center = center.astype(int)
-        cv2.circle(img,center,10,(0,0,255),5)
+            # find and draw center
+            center = centroids[i]
+            center = np.round(center)
+            center = center.astype(int)
+            cv2.circle(img, center, 7, (0, 0, 255), 2)
 
-        # find and draw bounding box
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3) 
+            # find and draw bounding box
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
         success = (num_final_labels == int(gt_list[c]))
 
         if success:
             print('We have found all', str(num_final_labels), '/',
-                  str(gt_list[c]), color_names[c], 'chewing gum balls. Yeah!')
-            foo = 0
+                  str(gt_list[c]), color_configuration[c][0]  , 'chewing gum balls. Yeah!')
         elif (num_final_labels > int(gt_list[c])):
             print('We have found too many (', str(num_final_labels), '/',
-                  str(gt_list[c]), ') candidates for', color_names[c], 'chewing gum balls. Damn!')
+                  str(gt_list[c]), ') candidates for', color_configuration[c][0], 'chewing gum balls. Damn!')
             all_colors_correct = False
         else:
             print('We have not found enough (', str(num_final_labels), '/',
-                  str(gt_list[c]), ') candidates for', color_names[c], 'chewing gum balls. Damn!')
+                  str(gt_list[c]), ') candidates for', color_configuration[c][0], 'chewing gum balls. Damn!')
             all_colors_correct = False
 
         # debug output of the test images
-        if ((img_name == 'images\chewing_gum_balls01.jpg')
-            or (img_name == 'images\chewing_gum_balls04.jpg')
-            or (img_name == 'images\chewing_gum_balls06.jpg')):
+        if ((img_name == 'Images\chewing_gum_balls01.jpg')
+            or (img_name == 'Images\chewing_gum_balls04.jpg')
+                or (img_name == 'Images\chewing_gum_balls06.jpg')):
             # show the original image with drawings in one window
             cv2.imshow('Original image', img)
+
             # show other images!
             cv2.imshow('Masked Image', mask)
-
-            cv2.waitKey(0)
+            # q for fast quit
+            if cv2.waitKey(0) == ord('q'):
+                break
             cv2.destroyAllWindows()
-
     if all_colors_correct:
         num_test_images_succeeded += 1
-        print ('Yeah, all colored objects have been found correctly in ',img_name)
+        print('Yeah, all colored objects have been found correctly in ', img_name)
 
-print ('Test result:', str(num_test_images_succeeded),'test images succeeded.')
-
-        
+print('Test result:', str(num_test_images_succeeded), 'test images succeeded.')
---
+++ GDV_tutorial_08_empty.py
@@ -0,0 +1,47 @@
+# Template matching, originally with objects from the image. Typical example is counting blood cells
+import cv2
+
+use_color = True
+
+if use_color:
+    # load image and template image, note that the template has been manually cut out of the image
+    img = cv2.imread('images\chewing_gum_balls06.jpg', cv2.IMREAD_COLOR)
+    template = cv2.imread('images\cgb_pink.jpg', cv2.IMREAD_COLOR)
+    # read shape of the template and original image
+    H, W, C = img.shape
+    h, w, c = template.shape
+    
+else:
+    # load image and template image, note that the template has been manually cut out of the image
+    img = cv2.imread('images\chewing_gum_gum_balls06.jpg', cv2.IMREAD_GRAYSCALE)
+    template = cv2.imread('images\cgb_pink.jpg', cv2.IMREAD_GRAYSCALE)
+    # read shape of the template and original image
+    H, W = img.shape
+    h, w = template.shape
+    
+# Define template matching methods, see https://docs.opencv.org/4.5.3/df/dfb/group__imgproc__object.html#ga3a7850640f1fe1f58fe91a2d7583695d for the math behind each method
+methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
+method_names = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+# loop over all methods in order to compare them
+for method, method_name in zip(methods, method_names):
+
+    # work on a new image each time
+    img2 = img.copy()
+    # do the template matching
+    result = cv2.matchTemplate(img2, template, method)
+    # get the best match location
+    min_Value, max_Value, min_location, max_location  = cv2.minMaxLoc(result)
+    print(min_Value, max_Value, min_location, max_location)
+    # draw rectangle at found location
+    cv2.rectangle(img2, min_location, (min_location[0] + w, min_location[1] + h), (0, 255, 0), 5)
+    cv2.rectangle(img2, max_location, (max_location[0] + w, max_location[1] + h), (255, 0, 0), 5)
+    # show original image with found location
+    cv2.imshow('Original', img2)
+    # show image with the template matching result for all pixels
+    result_title = 'Result with %s' % method_name
+    cv2.imshow(result_title, result)
+    cv2.waitKey(0)
+    cv2.destroyWindow(result_title)
+    
+cv2.destroyAllWindows()
